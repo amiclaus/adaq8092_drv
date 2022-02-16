8fe3bf323eb4514d198a02843522ba309041115c
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-02-16 16:46:38 +0200

    adaq8092: initial commit
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

---
+++ adaq8092.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * ADAQ9082 driver
+ *
+ * Copyright 2022 Analog Devices Inc.
+ */
+
+#include <linux/bitfield.h>
+#include <linux/bits.h>
+#include <linux/device.h>
+#include <linux/iio/iio.h>
+#include <linux/module.h>
+#include <linux/regmap.h>
+#include <linux/regulator/consumer.h>
+#include <linux/spi/spi.h>
