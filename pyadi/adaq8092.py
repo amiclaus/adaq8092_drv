c01388c3b23eb4e63f2374b6b9cfb6812c74a411
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-07-14 18:00:06 +0300

    pyadi: update device class

--- pyadi/adaq8092.py
+++ pyadi/adaq8092.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 Analog Devices, Inc.
+# Copyright (C) 2022 Analog Devices, Inc.
 #
 # All rights reserved.
 #
@@ -44,10 +44,10 @@ class adaq8092(rx, context_manager):
 
     """ ADAQ8092 14-Bit, 105MSPS, Dual-Channel uModule Data Acquisition Solution  """
 
-    _device_name = " "
+    _device_name = "adaq8092"
 
     def __init__(
-        self, uri="ip:analog.local",
+        self, uri="",
     ):
         """Initialize."""
         context_manager.__init__(self, uri, self._device_name)
@@ -61,7 +61,7 @@ class adaq8092(rx, context_manager):
             self._device_name = "cf_axi_adc"
 
         rx._rx_stack_interleaved = True  # DMA config. dependent. True for ad7768.
-        rx._rx_data_type = np.int32
+        rx._rx_data_type = np.int16
         for ch in self._rxadc.channels:
             name = ch._id
             self._rx_channel_names.append(name)
@@ -70,18 +70,18 @@ class adaq8092(rx, context_manager):
     @property
     def alt_bit_pol_en_available(self):
         """Get available Alternate Bit Polarity Mode Control."""
-        return self._get_iio_dev_attr("alt_bit_pol_en_available")
+        return self._get_iio_dev_attr_str("alt_bit_pol_en_available")
 
     @property
     def alt_bit_pol_en(self):
         """Get Alternate Bit Polarity Mode Control."""
-        return self._get_iio_dev_attr("alt_bit_pol_en")
+        return self._get_iio_dev_attr_str("alt_bit_pol_en")
 
     @alt_bit_pol_en.setter
     def alt_bit_pol_en(self, rate):
         """Set Alternate Bit Polarity Mode Control."""
         if rate in self.alt_bit_pol_en_available:
-            self._set_iio_dev_attr("alt_bit_pol_en", rate)
+            self._set_iio_dev_attr_str("alt_bit_pol_en", rate)
         else:
             raise ValueError(
                 "Error: Alternate Bit Polarity Mode Control not supported \nUse one of: "
@@ -91,18 +91,18 @@ class adaq8092(rx, context_manager):
     @property
     def clk_dc_mode_available(self):
         """Get available Clock Duty Cycle Stabilizer."""
-        return self._get_iio_dev_attr("clk_dc_mode_available")
+        return self._get_iio_dev_attr_str("clk_dc_mode_available")
 
     @property
     def clk_dc_mode(self):
         """Get Clock Duty Cycle Stabilizer."""
-        return self._get_iio_dev_attr("clk_dc_mode")
+        return self._get_iio_dev_attr_str("clk_dc_mode")
 
     @clk_dc_mode.setter
     def clk_dc_mode(self, rate):
         """Set Clock Duty Cycle Stabilizer."""
         if rate in self.clk_dc_mode_available:
-            self._set_iio_dev_attr("clk_dc_mode", rate)
+            self._set_iio_dev_attr_str("clk_dc_mode", rate)
         else:
             raise ValueError(
                 "Error: Clock Duty Cycle Stabilizer not supported \nUse one of: "
@@ -112,18 +112,18 @@ class adaq8092(rx, context_manager):
     @property
     def clk_phase_mode_available(self):
         """Get available Output Clock Phase Delay."""
-        return self._get_iio_dev_attr("clk_phase_mode_available")
+        return self._get_iio_dev_attr_str("clk_phase_mode_available")
 
     @property
     def clk_phase_mode(self):
         """Get Output Clock Phase Delay."""
-        return self._get_iio_dev_attr("clk_phase_mode")
+        return self._get_iio_dev_attr_str("clk_phase_mode")
 
     @clk_phase_mode.setter
     def clk_phase_mode(self, rate):
         """Set Output Clock Phase Delay."""
         if rate in self.clk_phase_mode_available:
-            self._set_iio_dev_attr("clk_phase_mode", rate)
+            self._set_iio_dev_attr_str("clk_phase_mode", rate)
         else:
             raise ValueError(
                 "Error: Output Clock Phase Delay not supported \nUse one of: "
@@ -133,18 +133,18 @@ class adaq8092(rx, context_manager):
     @property
     def clk_pol_mode_available(self):
         """Get available CLKOUT Polarity."""
-        return self._get_iio_dev_attr("clk_pol_mode_available")
+        return self._get_iio_dev_attr_str("clk_pol_mode_available")
 
     @property
     def clk_pol_mode(self):
         """Get CLKOUT Polarity."""
-        return self._get_iio_dev_attr("clk_pol_mode")
+        return self._get_iio_dev_attr_str("clk_pol_mode")
 
     @clk_pol_mode.setter
     def clk_pol_mode(self, rate):
         """Set CLKOUT Polarity."""
         if rate in self.clk_pol_mode_available:
-            self._set_iio_dev_attr("clk_pol_mode", rate)
+            self._set_iio_dev_attr_str("clk_pol_mode", rate)
         else:
             raise ValueError(
                 "Error: CLKOUT Polarity not supported \nUse one of: "
@@ -154,18 +154,18 @@ class adaq8092(rx, context_manager):
     @property
     def data_rand_en_available(self):
         """Get available Data Randomizer."""
-        return self._get_iio_dev_attr("data_rand_en_available")
+        return self._get_iio_dev_attr_str("data_rand_en_available")
 
     @property
     def data_rand_en(self):
         """Get Data Randomizer."""
-        return self._get_iio_dev_attr("data_rand_en")
+        return self._get_iio_dev_attr_str("data_rand_en")
 
     @data_rand_en.setter
     def data_rand_en(self, rate):
         """Set Data Randomizer."""
         if rate in self.data_rand_en_available:
-            self._set_iio_dev_attr("data_rand_en", rate)
+            self._set_iio_dev_attr_str("data_rand_en", rate)
         else:
             raise ValueError(
                 "Error: Data Randomizer not supported \nUse one of: "
@@ -175,18 +175,18 @@ class adaq8092(rx, context_manager):
     @property
     def dout_en_available(self):
         """Get available Digital Outputs."""
-        return self._get_iio_dev_attr("dout_en_available")
+        return self._get_iio_dev_attr_str("dout_en_available")
 
     @property
     def dout_en(self):
         """Get Digital Outputs."""
-        return self._get_iio_dev_attr("dout_en")
+        return self._get_iio_dev_attr_str("dout_en")
 
     @dout_en.setter
     def dout_en(self, rate):
         """Set Digital Outputs."""
         if rate in self.dout_en_available:
-            self._set_iio_dev_attr("dout_en", rate)
+            self._set_iio_dev_attr_str("dout_en", rate)
         else:
             raise ValueError(
                 "Error: Digital Outputs not supported \nUse one of: "
@@ -196,18 +196,18 @@ class adaq8092(rx, context_manager):
     @property
     def dout_mode_available(self):
         """Get available Digital Output Mode."""
-        return self._get_iio_dev_attr("dout_mode_available")
+        return self._get_iio_dev_attr_str("dout_mode_available")
 
     @property
     def dout_mode(self):
         """Get Digital Output Mode."""
-        return self._get_iio_dev_attr("dout_mode")
+        return self._get_iio_dev_attr_str("dout_mode")
 
     @dout_mode.setter
     def dout_mode(self, rate):
         """Set Digital Output Mode."""
         if rate in self.dout_mode_available:
-            self._set_iio_dev_attr("dout_mode", rate)
+            self._set_iio_dev_attr_str("dout_mode", rate)
         else:
             raise ValueError(
                 "Error: Digital Output Mode not supported \nUse one of: "
@@ -217,18 +217,18 @@ class adaq8092(rx, context_manager):
     @property
     def lvds_cur_mode_available(self):
         """Get available LVDS Output Current."""
-        return self._get_iio_dev_attr("lvds_cur_mode_available")
+        return self._get_iio_dev_attr_str("lvds_cur_mode_available")
 
     @property
     def lvds_cur_mode(self):
         """Get LVDS Output Current."""
-        return self._get_iio_dev_attr("lvds_cur_mode")
+        return self._get_iio_dev_attr_str("lvds_cur_mode")
 
     @lvds_cur_mode.setter
     def lvds_cur_mode(self, rate):
         """Set LVDS Output Current."""
         if rate in self.lvds_cur_mode_available:
-            self._set_iio_dev_attr("lvds_cur_mode", rate)
+            self._set_iio_dev_attr_str("lvds_cur_mode", rate)
         else:
             raise ValueError(
                 "Error: LVDS Output Current not supported \nUse one of: "
@@ -238,18 +238,18 @@ class adaq8092(rx, context_manager):
     @property
     def lvds_term_mode_available(self):
         """Get available LVDS Internal Termination."""
-        return self._get_iio_dev_attr("lvds_term_mode_available")
+        return self._get_iio_dev_attr_str("lvds_term_mode_available")
 
     @property
     def lvds_term_mode(self):
         """Get LVDS Internal Termination."""
-        return self._get_iio_dev_attr("lvds_term_mode")
+        return self._get_iio_dev_attr_str("lvds_term_mode")
 
     @lvds_term_mode.setter
     def lvds_term_mode(self, rate):
         """Set LVDS Internal Termination."""
         if rate in self.lvds_term_mode_available:
-            self._set_iio_dev_attr("lvds_term_mode", rate)
+            self._set_iio_dev_attr_str("lvds_term_mode", rate)
         else:
             raise ValueError(
                 "Error: LVDS Internal Termination not supported \nUse one of: "
@@ -259,27 +259,48 @@ class adaq8092(rx, context_manager):
     @property
     def par_ser_gpio(self):
         """Get Paraller/Serial Gpio Value."""
-        return self._get_iio_dev_attr("par_ser_gpio")
+        return self._get_iio_dev_attr_str("par_ser_gpio")
 
     @property
     def pd_gpio_available(self):
         """Get available Power Down GPIO Configuration."""
-        return self._get_iio_dev_attr("pd_gpio_available")
+        return self._get_iio_dev_attr_str("pd_gpio_available")
 
     @property
     def pd_gpio(self):
         """Get Power Down GPIO Configuration."""
-        return self._get_iio_dev_attr("pd_gpio")
+        return self._get_iio_dev_attr_str("pd_gpio")
 
     @pd_gpio.setter
     def pd_gpio(self, rate):
         """Set Power Down GPIO Configuration."""
         if rate in self.pd_gpio_available:
-            self._set_iio_dev_attr("pd_gpio", rate)
+            self._set_iio_dev_attr_str("pd_gpio", rate)
         else:
             raise ValueError(
                 "Error: Power Down GPIO Configuration not supported \nUse one of: "
                 + str(self.pd_gpio_available)
+            )
+
+    @property
+    def pd_mode_available(self):
+        """Get available Power Down Modes."""
+        return self._get_iio_dev_attr_str("pd_mode_available")
+
+    @property
+    def pd_mode(self):
+        """Get Power Down Mode."""
+        return self._get_iio_dev_attr_str("pd_mode")
+
+    @pd_mode.setter
+    def pd_mode(self, rate):
+        """Set Power Down Mode."""
+        if rate in self.pd_mode_available:
+            self._set_iio_dev_attr_str("pd_mode", rate)
+        else:
+            raise ValueError(
+                "Error: Digital Output Test Pattern not supported \nUse one of: "
+                + str(self.pd_mode_available)
             )
 
     @property
@@ -295,18 +316,18 @@ class adaq8092(rx, context_manager):
     @property
     def test_mode_available(self):
         """Get available Digital Output Test Pattern."""
-        return self._get_iio_dev_attr("test_mode_available")
+        return self._get_iio_dev_attr_str("test_mode_available")
 
     @property
     def test_mode(self):
         """Get Digital Output Test Pattern."""
-        return self._get_iio_dev_attr("test_mode")
+        return self._get_iio_dev_attr_str("test_mode")
 
     @test_mode.setter
     def test_mode(self, rate):
         """Set Digital Output Test Pattern."""
         if rate in self.test_mode_available:
-            self._set_iio_dev_attr("test_mode", rate)
+            self._set_iio_dev_attr_str("test_mode", rate)
         else:
             raise ValueError(
                 "Error: Digital Output Test Pattern not supported \nUse one of: "
@@ -316,18 +337,18 @@ class adaq8092(rx, context_manager):
     @property
     def twos_complement_available(self):
         """Get available Two's Complement Modes. """
-        return self._get_iio_dev_attr("twos_complement_available")
+        return self._get_iio_dev_attr_str("twos_complement_available")
 
     @property
     def twos_complement(self):
         """Get Two's Complement Modes."""
-        return self._get_iio_dev_attr("twos_complement")
+        return self._get_iio_dev_attr_str("twos_complement")
 
-    @test_mode.setter
+    @twos_complement.setter
     def twos_complement(self, rate):
         """Set Two's Complement Modes."""
         if rate in self.twos_complement_available:
-            self._set_iio_dev_attr("twos_complement", rate)
+            self._set_iio_dev_attr_str("twos_complement", rate)
         else:
             raise ValueError(
                 "Error: Two's Complement Modes not supported \nUse one of: "
